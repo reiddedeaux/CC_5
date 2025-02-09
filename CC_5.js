cc148960833bfd697280cd83163f28adca961a16
reiddedeaux <rdedeaux@usf.edu>
2025-02-09 15:34:40 -0500

    Task 1 - Customer Profile

---
+++ CC_5.js
@@ -0,0 +1,7 @@
+// Task 1 Customer Profile
+let customer = {
+    name: "John Doe",
+    age: 35 ,
+    email: "john.doe@email.com"
+};
+console.log(customer) ; // declaring customer with the details of them and logging it into the console
