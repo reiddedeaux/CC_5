1133b63b92315708b5cfceeba555ef6d82ab0a2f
reiddedeaux <rdedeaux@usf.edu>
2025-02-09 15:44:54 -0500

    Task 2 - Order Details

--- CC_5.js
+++ CC_5.js
@@ -8,3 +8,19 @@ let customer = {
  console.log(`Age: ${customer.age}`);
  console.log(`Email: ${customer.email}`); 
  // logging all the values into the console
+
+ // Task 2 - Order Details
+
+ let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+
+    displayOrder: function(){
+        console.log(`Order ID: ${this.orderID} Amount: $${this.totalAmount}, Status: $${this.status}`);
+    
+    }
+ } ; // Logging the order details into the console
+ order.displayOrder();
+
+ 
